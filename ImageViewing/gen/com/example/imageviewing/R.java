964846a4705fe67c75d7766a08297f9d99112fe2
AlfianFaiz <faiz.noin@gmail.com>
2015-05-19 23:23:33 +0700

    Final Commit :)

--- ImageViewing/gen/com/example/imageviewing/R.java
+++ ImageViewing/gen/com/example/imageviewing/R.java
@@ -1080,6 +1080,8 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020057;
     }
     public static final class id {
+        public static final int ImageButton=0x7f09003c;
+        public static final int TitleTextView=0x7f09003d;
         public static final int action_bar=0x7f09001c;
         public static final int action_bar_activity_content=0x7f090001;
         public static final int action_bar_container=0x7f09001b;
@@ -1091,7 +1093,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f090002;
         public static final int action_menu_presenter=0x7f090003;
         public static final int action_mode_close_button=0x7f090024;
-        public static final int action_settings=0x7f09003c;
+        public static final int action_settings=0x7f09003e;
         public static final int activity_chooser_view_content=0x7f090025;
         public static final int always=0x7f09000f;
         public static final int beginning=0x7f090016;
--- ImageViewing/src/com/example/imageviewing/MainActivity.java
+++ ImageViewing/src/com/example/imageviewing/MainActivity.java
@@ -1,34 +1,98 @@
 package com.example.imageviewing;
 
-import android.support.v7.app.ActionBarActivity;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.provider.MediaStore;
+import android.provider.MediaStore.Audio.Media;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
-
+	public final static int DISPLAYWIDTH = 200;
+	public final static int DISPLAYHEIGHT = 200;
+	TextView titleTextView;
+	ImageButton imageButton;
+	Cursor cursor;
+	Bitmap bmp;
+	String imageFilePath;
+	int fileColumn;
+	int titleColumn;
+	int displayColumn;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	
+		titleTextView = (TextView) this.findViewById(R.id.TitleTextView);
+		imageButton = (ImageButton) this.findViewById(R.id.ImageButton);
+		
+		String[] columns = { Media.DATA, Media._ID, Media.TITLE, Media.DISPLAY_NAME };
+		cursor = managedQuery(Media.EXTERNAL_CONTENT_URI, columns, null, null, null);
+		
+		fileColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+		titleColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.TITLE);
+		displayColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME);
+		
+		if (cursor.moveToFirst()) {
+			//titleTextView.setText(cursor.getString(titleColumn));
+			titleTextView.setText(cursor.getString(displayColumn));
+			imageFilePath = cursor.getString(fileColumn);
+			bmp = getBitmap(imageFilePath);
+			// Display it
+			imageButton.setImageBitmap(bmp);
+			}
+		
+		imageButton.setOnClickListener(
+				new OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						// TODO Auto-generated method stub
+						if (cursor.moveToNext())
+						{
+							//titleTextView.setText(cursor.getString(titleColumn));
+							titleTextView.setText(cursor.getString(displayColumn));
+							imageFilePath = cursor.getString(fileColumn);
+							bmp = getBitmap(imageFilePath);
+							imageButton.setImageBitmap(bmp);
+						}
+					}
+				});
+		
+		
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	private Bitmap getBitmap(String imageFilePath) {
+		BitmapFactory.Options bmpfactoryoption = new BitmapFactory.Options();
+		bmpfactoryoption.inJustDecodeBounds = true;
+		Bitmap bmp = BitmapFactory.decodeFile(imageFilePath,bmpfactoryoption);
+		int heightRatio = (int) Math.ceil(bmpfactoryoption.outHeight/(float) DISPLAYHEIGHT);
+				int widthRatio = (int) Math.ceil(bmpfactoryoption.outWidth/(float) DISPLAYWIDTH);
+				Log.v("HEIGHTRATIO", "" + heightRatio);
+				Log.v("WIDTHRATIO", "" + widthRatio);
+				// If both of the ratios are greater than 1, one of the sides of
+				// the image is greater than the screen
+				if (heightRatio > 1 && widthRatio > 1) {
+				if (heightRatio > widthRatio) {
+				// Height ratio is larger, scale according to it
+				bmpfactoryoption.inSampleSize = heightRatio;
+				} else {
+				// Width ratio is larger, scale according to it
+				bmpfactoryoption.inSampleSize = widthRatio;
+				}
+				}
+				// Decode it for real
+				bmpfactoryoption.inJustDecodeBounds = false;
+				bmp = BitmapFactory.decodeFile(imageFilePath, bmpfactoryoption);
+				return bmp;
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+	
 }
